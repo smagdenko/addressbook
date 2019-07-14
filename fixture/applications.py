da5a2ad8e6bf8513941130858c01f307dee5cf9b
smagdenko <magdenko_ne@ukr.net>
2019-07-14 22:23:18 +0300

    Add methods: is_loged_as, ensure_login, ensure_logout

--- fixture/applications.py
+++ fixture/applications.py
@@ -4,7 +4,7 @@ class Applications:
 
     def __init__(self, driver):
         self.driver=driver
-        self.driver.implicitly_wait(5)
+        # self.driver.implicitly_wait(5)
         self.group=GroupSession(self)
 
     def open_homepage(self):
@@ -20,9 +20,31 @@ class Applications:
         driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
 
+    def is_loged_in(self):
+        driver = self.driver
+        return len(driver.find_elements_by_link_text("Logout"))>0
+
+    def is_loged_as(self, user):
+        driver = self.driver
+        return driver.find_element_by_xpath(".//*[@id='top']//b").text == ("("+ user +")")
+
+    def ensure_login(self, user, password):
+        driver = self.driver
+        if self.is_loged_in():
+            if self.is_loged_as(user):
+                return
+            else:
+                self.logout()
+        self.login(user, password)
+
     def logout(self):
         driver = self.driver
         driver.find_element_by_link_text("Logout").click()
+
+    def ensure_logout(self):
+        driver = self.driver
+        if len(driver.find_elements_by_link_text("Logout"))>0:
+            self.logout()
 
     def destroy(self):
         self.driver.quit()
