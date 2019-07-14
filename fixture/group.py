9cabc31688d2c1c9d0641f42482800ae24bc1645
smagdenko <magdenko_ne@ukr.net>
2019-07-14 22:25:44 +0300

    Add  new methods "open_group_page" and "count"

--- fixture/group.py
+++ fixture/group.py
@@ -1,5 +1,3 @@
-from selenium import webdriver
-
 class GroupSession:
 
     def __init__(self, app):
@@ -20,7 +18,6 @@ class GroupSession:
         self.change_field_text("group_footer", group.footer)
         driver.find_element_by_xpath(".//*[@type=\"submit\"]").click()
 
-
     def change_field_text(self, field_name, text):
         driver = self.app.driver
         if text is not None:
@@ -28,11 +25,16 @@ class GroupSession:
             driver.find_element_by_name(field_name).clear()
             driver.find_element_by_name(field_name).send_keys(text)
 
+    def open_group_page(self):
+        driver = self.app.driver
+        if not (driver.current_url.endswith("/groups") and len(driver.find_elements_by_xpath(".//*[@ name=\"new\"]"))>0):
+            driver.find_element_by_xpath("//*[@id='nav']//a[contains(text(),'groups')]").click()
 
     def delete(self):
         driver = self.app.driver
-        driver.find_element_by_xpath(".//*[@id='nav']/ul/li[3]/a").click()
+        self.open_group_page()
         groups=driver.find_elements_by_xpath("//*[@id='content']//span")
+
         for _ in range(len(groups)):
             driver.find_element_by_xpath("//*[@id='content']//span[1]").click()
             driver.find_element_by_xpath("//input[2][@name='delete']").click()
@@ -44,5 +46,9 @@ class GroupSession:
         driver.find_element_by_xpath(".//*[@id='content']/form/span[1]").click()
         driver.find_element_by_xpath("//input[3][@name='edit']").click()
         self.fill_group_name(new_group_data)
-
         driver.find_element_by_xpath(".//*[@id='content']//a[contains(.,'group page')]").click()
+
+    def count(self):
+        driver = self.app.driver
+        self.open_group_page()
+        return len(driver.find_elements_by_xpath("//*[@name=\"selected[]\"]"))
