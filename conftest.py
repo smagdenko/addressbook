68066d13c5296ae3e3c1f43a8ee300275c055994
smagdenko <magdenko_ne@ukr.net>
2019-07-14 22:24:36 +0300

    Add a new fixture and method "stop"

--- conftest.py
+++ conftest.py
@@ -4,12 +4,11 @@ from selenium import webdriver
 
 fixture = None
 
-
 @pytest.yield_fixture(scope='session')
 def driver():
     _driver = webdriver.Chrome()
     yield _driver
-    _driver.quit()
+    # _driver.quit()
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -17,9 +16,17 @@ def app(driver):
     global fixture
     if fixture is None:
         fixture = Applications(driver)
-        fixture.login(user="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Applications(driver)
-            fixture.login(user="admin", password="secret")
+    fixture.ensure_login(user="admin", password="secret")
+    return fixture
+
+
+@pytest.fixture(scope='session', autouse=True)
+def stop(request):
+    def fin():
+        fixture.ensure_logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
     return fixture
