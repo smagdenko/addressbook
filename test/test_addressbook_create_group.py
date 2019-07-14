f3dd9bec402508ae6424a0bfa95c31869388fc2f
smagdenko <magdenko_ne@ukr.net>
2019-07-14 22:28:54 +0300

    improve a scenario

--- test/test_addressbook_create_group.py
+++ test/test_addressbook_create_group.py
@@ -1,21 +1,9 @@
-
 from model.group import Group
-
-
-# @pytest.fixture
-# def app(request):
-#     fixture = Applications()
-#     # request.addfinalizer(fixture.destroy())
-#     return fixture
 
 
 def test_add_group(app):
     app.group.create(Group(name="new", header="bbbbbbbbbbb", footer="vcmvncmvn"))
     # app.group.delete()
     app.logout()
-
-
-
-
 
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,5 +2,5 @@ from model.group import Group
 
 
 def test_modify_group(app):
-    app.group.modify_first_group(Group(name="New group"))
-    app.logout()
+    app.group.modify_first_group(Group(name="New group1"))
+    # app.logout()
