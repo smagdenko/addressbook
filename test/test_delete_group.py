4161ef1d0ee43ca99e3bd09b90ee036306aa1df4
smagdenko <magdenko_ne@ukr.net>
2019-07-15 22:16:34 +0300

    Add new assert for <test_delete_group> metod

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 from fixture.group import GroupSession
 
+
 def test_delete_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name='create for deleting'))
-    app.group.delete()
+    old_groups = app.group.get_groups_list()
+    app.group.delete_first_group()
+    new_groups = app.group.get_groups_list()
+    assert len(old_groups)-1 == len(new_groups)
+    old_groups[0:1] = []    # delete first element of old_group list
+    assert old_groups == new_groups
