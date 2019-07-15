2e88299e9807a40140e831e9043c3d888e7de5b8
smagdenko <magdenko_ne@ukr.net>
2019-07-15 16:58:45 +0300

    Add assert to method "open_group_page"

--- test/test_addressbook_create_group.py
+++ test/test_addressbook_create_group.py
@@ -2,8 +2,9 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_groups_list()
     app.group.create(Group(name="new", header="bbbbbbbbbbb", footer="vcmvncmvn"))
-    # app.group.delete()
-    app.logout()
+    new_groups = app.group.get_groups_list()
+    assert len(old_groups)+1 == len(new_groups)
 
 
