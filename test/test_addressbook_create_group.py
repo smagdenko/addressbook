1ba3a360e4caf253fe3efee9cd4304065dee6273
smagdenko <magdenko_ne@ukr.net>
2019-07-15 22:15:31 +0300

    Add method <test_add_groups> using RandomWords

--- test/test_addressbook_create_group.py
+++ test/test_addressbook_create_group.py
@@ -1,10 +1,21 @@
 from model.group import Group
+from random_word import RandomWords
 
 
 def test_add_group(app):
+    r = RandomWords()
+    word = r.get_random_word()
     old_groups = app.group.get_groups_list()
-    app.group.create(Group(name="new", header="bbbbbbbbbbb", footer="vcmvncmvn"))
+    app.group.create(Group(name=word, header=word, footer=word))
     new_groups = app.group.get_groups_list()
-    assert len(old_groups)+1 == len(new_groups)
+    assert len(old_groups) + 1 == len(new_groups)
 
+def test_add_groups(app):
+    r = RandomWords()
+    for _ in range(3):
+        word = r.get_random_word()
+        old_groups = app.group.get_groups_list()
+        app.group.create(Group(name=word, header=word, footer=word))
+        new_groups = app.group.get_groups_list()
+        assert len(old_groups)+1 == len(new_groups)
 
