7e55673d644ce77aa71973c5691f761f3f861d12
smagdenko <magdenko_ne@ukr.net>
2019-07-19 09:09:48 +0300

    Add assertion for <test_add_group>

--- test/test_addressbook_create_group.py
+++ test/test_addressbook_create_group.py
@@ -1,3 +1,5 @@
+from sys import maxsize
+
 from model.group import Group
 from random_word import RandomWords
 
@@ -5,10 +7,16 @@ from random_word import RandomWords
 def test_add_group(app):
     r = RandomWords()
     word = r.get_random_word()
+    group = (Group(name=word, header=word, footer=word))
     old_groups = app.group.get_groups_list()
-    app.group.create(Group(name=word, header=word, footer=word))
+    app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_groups_list()
-    assert len(old_groups) + 1 == len(new_groups)
+
+    old_groups.append(group)
+
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 def test_add_groups(app):
     r = RandomWords()
