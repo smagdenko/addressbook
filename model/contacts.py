5fe39ce8e452c5021a1d513ba26e013c12e60be1
smagdenko <magdenko_ne@ukr.net>
2019-07-25 08:34:16 +0300

    Add class <Contacts>

---
+++ model/contacts.py
@@ -0,0 +1,7 @@
+class Contacts:
+
+    def __init__(self, first_name=None, last_name=None, mobile=None, id=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.mobile = mobile
+        self.id = id
