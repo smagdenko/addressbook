958543e1e437b1334ba3ab4ec395ecc681487655
smagdenko <magdenko_ne@ukr.net>
2019-07-26 17:51:20 +0300

    Create a method <test_phone_on_home_page>

---
+++ test/test_get_info_edit_page.py
@@ -0,0 +1,9 @@
+
+def test_phone_on_home_page(app):
+
+    contact_from_home_page = app.contact.get_contacts_list()[0]
+    contact_from_edit_page = app.contact.get_info_from_edit_page(0)
+
+    assert contact_from_edit_page.email_1 == contact_from_home_page.email_1
+    assert contact_from_edit_page.email_2 == contact_from_home_page.email_2
+    assert contact_from_edit_page.email_3 == contact_from_home_page.email_3
