4e583ba4080b909039d044697993d98dddb3cda3
smagdenko <magdenko_ne@ukr.net>
2019-07-26 17:47:43 +0300

    Modify <get_contacts_list> Create <get_info_from_edit_page>

--- fixture/contacts_page.py
+++ fixture/contacts_page.py
@@ -35,15 +35,17 @@ class ContactsHelper:
     contact_cache = None
 
     def get_contacts_list(self):
-        driver = self.app.driver
-        self.open_contacts_page()
-        contacts = []
-        for element in driver.find_elements_by_xpath('.//*[@name="entry"]'):
-            firstname = element[3].text
-            lastname = element[2].text
-            # id = element.find_element_by_name("selected[]").get_attribute("value")
-            id = element[0].find_element_by_name("input").get_attribute("value")
-            self.contact_cache.append(Contacts(firstname=firstname, lastname=lastname, id=id))
+        if self.contact_cache is None:
+            driver = self.app.driver
+            # self.open_contacts_page()
+            self.contact_cache = []
+            for element in driver.find_elements_by_xpath('//*[@name="entry"]'):
+                firstname = element[3].text
+                lastname = element[2].text
+                id = element[0].find_element_by_name("input").get_attribute("value")
+                all_mails = element[5].text.splitlines()
+                self.contact_cache.append(Contacts(firstname=firstname, lastname=lastname, id=id,
+                                                    email_1=all_mails[0], email_2=all_mails[1], email_3=all_mails[2]))
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self, index):
@@ -51,5 +53,17 @@ class ContactsHelper:
         row = driver.find_elements_by_xpath(".//*[@id='maintable']//td[7]")[index]
         cell = row.find_element_by_name("entry")
         cell.find_element_by_tag_name('a').click()
+
+    def get_info_from_edit_page(self, index):
+        driver = self.app.driver
+        self.open_contact_to_edit_by_index(index)
+        firstname = driver.find_element_by_name("firstname").attribute("value")
+        lastname = driver.find_element_by_name("lastname").attribute("value")
+        email_1 = driver.find_element_by_name("email").attribute("value")
+        email_2 = driver.find_element_by_name("email2").attribute("value")
+        email_3 = driver.find_element_by_name("email3").attribute("value")
+        return Contacts(firstname=firstname, lastname=lastname, email_1=email_1, email_2=email_2, email_3=email_3)
+
+
 
 
