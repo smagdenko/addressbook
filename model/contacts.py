af0b6ddfb74a328c8fe2f6e0938d6178da759950
smagdenko <magdenko_ne@ukr.net>
2019-07-29 23:06:52 +0300

    Modify <__init__> method

--- model/contacts.py
+++ model/contacts.py
@@ -1,13 +1,13 @@
 class Contacts:
 
-    def __init__(self, first_name=None, last_name=None, mobile=None, id=None, mails=None): #, mail_1=None, mail_2=None, mail_3=None):
+    def __init__(self, first_name=None, last_name=None, mobile=None, id=None, email_1=None, email_2=None, email_3=None):
         self.first_name = first_name
         self.last_name = last_name
         self.mobile = mobile
         self.id = id
-        self.mail_1 = mails[1]
-        self.mail_2 = mails[2]
-        self.mail_3 = mails[3]
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.email_3 = email_3
 
     def __eq__(self, other):
         return (self.id == other.id or self.id is None or other.id is None) \
