da7bcbfe7d5f565b4eb857dcdddd2a9699e5e5e6
smagdenko <magdenko_ne@ukr.net>
2019-08-04 22:25:38 +0300

    Add  <test_phones_on_home_page>

--- test/test_get_info_edit_page.py
+++ test/test_get_info_edit_page.py
@@ -1,5 +1,5 @@
 
-def test_phone_on_home_page(app):
+def test_contacts_on_home_page(app):
 
     contact_from_home_page = app.contact.get_contacts_list()[1]
     contact_from_edit_page = app.contact.get_info_from_edit_page(1)
@@ -7,3 +7,13 @@ def test_phone_on_home_page(app):
     assert contact_from_edit_page.email_1 == contact_from_home_page.email_1
     assert contact_from_edit_page.email_2 == contact_from_home_page.email_2
     assert contact_from_edit_page.email_3 == contact_from_home_page.email_3
+
+
+def test_phones_on_home_page(app):
+
+    phones_from_home_page = app.contact.get_contacts_list()[1]
+    phones_from_edit_page = app.contact.get_info_from_edit_page(1)
+
+    assert phones_from_edit_page.work_phone == phones_from_home_page.work_phone
+    assert phones_from_edit_page.home_phone == phones_from_home_page.home_phone
+    assert phones_from_edit_page.mobile_phone == phones_from_home_page.mobile_phone
