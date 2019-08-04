a7cbfe39a77ed0b87941b3b236654736218ea45d
smagdenko <magdenko_ne@ukr.net>
2019-08-04 22:24:43 +0300

    Add  random generator for phones

--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -1,20 +1,25 @@
-from random_word import RandomWords
+from random_words import RandomWords
 from model.contacts import Contacts
 from random import randint
 
 
 def test_create_contact(app):
     r = RandomWords()
-    phone = "+38093" + str(randint(0, 9999999))
 
-    words_list = r.get_random_words()
+    phones=[]
+    for _ in range(0,3):
+        ph = "+38093" + str(randint(0, 9999999))
+        phones.append(ph)
+
+    words_list = r.random_words(count=5)
     value = []
     for w in words_list:
         value.append(str(w))
 
-    mail_list = r.get_random_words()
+    mail_list = r.random_words(count=5)
     mails = []
     for mail in mail_list:
         mails.append(str(mail) + "@mailer.com")
-    contact = Contacts(first_name=value[1], last_name=value[2], mobile=phone, mail_1=mails[1])
+    contact = Contacts(first_name=value[1], last_name=value[2], mobile_phone=phones[0],
+                       home_phone=phones[1], work_phone=phones[2], email_1=mails[1])
     app.contact.create(contact)
